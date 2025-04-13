a7dae5dafdd25d32355e6bf688a7cc0119cadf63
o-uki <makaroniaji024@gmail.com>
2025-04-13 22:40:27 +0900

    文字の出現頻度のデータをゲットするようにしました。

--- face.py
+++ face.py
@@ -6,10 +6,24 @@ from string import Template
 # 顔文字のリスト
 face_datas = []
 
+# 文字の出現量のリスト
+character_datas = pd.DataFrame({"character_data": [], "count": []})
+
 # 顔文字のデータファイル
 face_data_file = pd.read_csv("./face_data.csv")
 for face_item in face_data_file:
+    # アルファベットが含まれていないか
     if not re.match("[a-zA-Z]+", face_item):
+        # 文字を一つずつチェック
+        for character in face_item:
+            contain_data = character_datas[character_datas["character_data"] == character]
+            if len(contain_data) > 0:
+                get_index = contain_data.index.values.tolist()[0]
+                add_amount = character_datas.loc[get_index, "count"]
+                character_datas.loc[get_index, "count"] = add_amount + 1
+            else:
+                character_datas = pd.concat([character_datas, pd.DataFrame({"character_data": [character], "count": [0]}, index=[len(character_datas)])])
+
         face_datas.append(face_item)
 
 # 文字列の入力を求める
